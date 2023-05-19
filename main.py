2a558a5394623b811420a75c88a2bf3ff97efaad
Saffy127 <88598834+Saffy127@users.noreply.github.com>
2023-05-19 03:58:35 +0000

    Made program more robust

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 API_Token = "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"
 
 from huggingface_hub.inference_api import InferenceApi
-inference = InferenceApi(repo_id="gpt2-large", token=API_Token)
+import json
 
-# defining our parameters
-top_p_params = {"max_length": 128, "top_p": 0.95, "do_sample":True} # top-p (nucleus sampling)
-top_k_params = {"max_length": 128,  "top_k": 4, "do_sample":True} # top-k
-contrastive_params = {"max_length": 128, "penalty_alpha": 0.6, "top_k": 4}
+try:
+  # Initialize the Hugging Face Inference API
+  inference = InferenceApi(repo_id="gpt2-large", token=API_Token)
 
-input_string = "Learn about AGI because"
-result = inference(input_string)
-print(result)
+  # defining our parameters
+  top_p_params = {
+    "max_length": 128,
+    "top_p": 0.95,
+    "do_sample": True
+  }  # top-p (nucleus sampling)
+  top_k_params = {"max_length": 128, "top_k": 4, "do_sample": True}  # top-k
+  contrastive_params = {"max_length": 128, "penalty_alpha": 0.6, "top_k": 4}
 
-result = inference(input_string, top_p_params)
-print("Top P: {}".format(result))
+  input_string = "Learn about AGI because"
 
-result = inference(input_string, top_k_params)
-print("Top K: {}".format(result))
+  # Perform inference with the default parameters
+  result = inference(input_string)
+  print(result)
 
+  # Perform inference with the top_p_params
+  result = inference(input_string, top_p_params)
+  print("Top P: {}".format(result))
 
-result = inference(input_string, contrastive_params)
-print(" Contrastive: {}".format(result))
+  # Perform inference with the top_k_params
+  result = inference(input_string, top_k_params)
+  print("Top K: {}".format(result))
+
+  # Perform inference with the contrastive_params
+  result = inference(input_string, contrastive_params)
+  print("Contrastive: {}".format(result))
+
+  # Save results to a file
+  with open('results.txt', 'w') as f:
+    f.write(json.dumps(result))
+
+except Exception as e:
+  print(f"An error occurred: {e}")
